7c9c6e504a5301ddcf761c947b6ea73b25275c65
evanandrewson <evanandrewson@gmail.com>
2019-07-26 09:14:42 -0700

    linked up form in product-entry.js

---
+++ src/product-entry.js
@@ -0,0 +1,29 @@
+import store from './data/store.js';
+
+const form = document.getElementById('product-entry');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const newProduct = {
+        code: formData.get('code'),
+        name: formData.get('name'),
+        image: formData.get('image'),
+        description: formData.get('description'),
+        category: formData.get('category'),
+        price: +formData.get('price'),
+        cost: +formData.get('cost')
+    }
+    store.addProduct(newProduct);
+    alert('product saved');
+    form.reset();
+});
+
+
+/*code: 'amethyst',
+name: 'Amethyst',
+image: 'assets/amethyst.jpg',
+description: 'purple and amazing',
+category: 'quartz',
+price: 5.00,
+cost: 3.00 */
